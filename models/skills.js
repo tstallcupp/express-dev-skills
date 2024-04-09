69b40e976def44fd4bd66b6961cba09fa36ce6cc
Taylor Stallcup <tstallcup321@gmail.com>
2024-04-09 16:27:05 -0500

    Created model/skills.js with data

---
+++ models/skills.js
@@ -0,0 +1,37 @@
+// Creat skills array
+const devSkills = [
+    { 
+      id: 1,
+      name: 'HTML',
+      description: 'Hypertext Markup Language for creating web pages.'
+    },
+    {
+      id: 2,
+      name: 'CSS',
+      description: 'Cascading Style Sheets for styling web pages.'
+    },
+    {
+      id: 3,
+      name: 'JavaScript',
+      description: 'A programming language used to create interactive effects within web browsers.'
+    },
+    {
+      id: 4,
+      name: 'Git',
+      description: 'A version control system for tracking changes in source code during software development.'
+    },
+    {
+      id: 5,
+      name: 'GitHub',
+      description: 'A web-based platform for version control using Git.'
+    }
+  ];
+
+  module.exports = {
+    getAll
+
+  };
+
+  function getAll() {
+    return devSkills;
+  }
